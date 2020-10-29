8e712e98a593b76d2828d8e1a1a72b266e55a3bd
jtbump <jtbump@iastate.edu>
2020-10-28 21:25:57 -0500

    Added logic for themes to go to score pages

--- app/src/main/java/com/example/matchthetiles/GlobalHighScores.java
+++ app/src/main/java/com/example/matchthetiles/GlobalHighScores.java
@@ -3,15 +3,38 @@ package com.example.matchthetiles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 public class GlobalHighScores extends AppCompatActivity {
 
+    private int score;
+    private String theme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_high_scores);
+
+        Intent intent = getIntent();
+        score = 0;
+        this.theme = intent.getStringExtra("theme");
+
+
+        switch(theme) {
+            case "winter":
+                findViewById(R.id.globalHighScoresLayout).setBackgroundColor(Color.GREEN);
+                break;
+            case "fall":
+                findViewById(R.id.globalHighScoresLayout).setBackgroundColor(Color.parseColor("#ffa500"));
+                break;
+            case "spring":
+                findViewById(R.id.globalHighScoresLayout).setBackgroundColor(Color.YELLOW);
+                break;
+            case "summer":
+                findViewById(R.id.globalHighScoresLayout).setBackgroundColor(Color.BLUE);
+                break;
+        }
     }
 
 
@@ -19,7 +42,7 @@ public class GlobalHighScores extends AppCompatActivity {
 
         //use this to go to next round
         Intent intent = new Intent(this,MainActivity.class);
-//        intent.putExtra("theme", theme);
+        intent.putExtra("theme", theme);
 //        intent.putExtra("myBestTime", myBestTime);
 //        intent.putExtra("globalBestTime", globalBestTime);
         startActivity(intent);
--- app/src/main/java/com/example/matchthetiles/MyHighScores.java
+++ app/src/main/java/com/example/matchthetiles/MyHighScores.java
@@ -3,22 +3,45 @@ package com.example.matchthetiles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 public class MyHighScores extends AppCompatActivity {
 
+    private int score;
+    private String theme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_high_scores);
+
+        Intent intent = getIntent();
+        score = 0;
+        this.theme = intent.getStringExtra("theme");
+
+
+        switch(theme) {
+            case "winter":
+                findViewById(R.id.myHighScoresLayout).setBackgroundColor(Color.GREEN);
+                break;
+            case "fall":
+                findViewById(R.id.myHighScoresLayout).setBackgroundColor(Color.parseColor("#ffa500"));
+                break;
+            case "spring":
+                findViewById(R.id.myHighScoresLayout).setBackgroundColor(Color.YELLOW);
+                break;
+            case "summer":
+                findViewById(R.id.myHighScoresLayout).setBackgroundColor(Color.BLUE);
+                break;
+        }
     }
 
     public void mainMenu(View v){
 
         //use this to go to next round
         Intent intent = new Intent(this,MainActivity.class);
-//        intent.putExtra("theme", theme);
+        intent.putExtra("theme", theme);
 //        intent.putExtra("myBestTime", myBestTime);
 //        intent.putExtra("globalBestTime", globalBestTime);
         startActivity(intent);
